c1d4b895dd35aca4ec2345c732b7f80f1ebc5593
DangD96 <35606061+DangD96@user.noreply.github.com>
2025-01-21 20:33:14 -0600

    test

--- src/test/java/DJDTest.java
+++ src/test/java/DJDTest.java
@@ -21,7 +21,9 @@ public class DJDTest {
         switch (browser) {
             case "Chrome":
                 ChromeOptions options = new ChromeOptions();
-                options.addArguments("--no-sandbox"); options.addArguments("--disable-dev-shm-usage"); options.addArguments("--headless");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                //options.addArguments("--headless");
                 // The name of the Docker selenium hub service is used as the hostname in the URL to access the Selenium Grid Hub
                 driver = new ChromeDriver(options);
                 break;
